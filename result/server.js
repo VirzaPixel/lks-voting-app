b53f173fe7c9bad557da21be25282ab1a2113454
VirzaPixezz <virzarizky151@gmail.com>
2026-03-10 19:29:26 +0700

    prod

--- result/server.js
+++ result/server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  // ubah
+  // edit
   connectionString: 'postgresql://admin123:LKSNCC2024@lks-rds.cunsvijdc0h4.us-east-1.rds.amazonaws.com/postgres'
 });
 
