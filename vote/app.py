b72432e0e605e9bff527d8612bcaee461ff4e7b8
VirzaPixezz <virzarizky151@gmail.com>
2026-03-10 17:43:27 +0700

    prod

--- vote/app.py
+++ vote/app.py
@@ -27,6 +27,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
+        # tanpa port
         g.redis = Redis(host="master.lks-redis.3ogtn5.use1.cache.amazonaws.com", db=0, socket_timeout=5)
     return g.redis
 
